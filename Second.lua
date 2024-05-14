c02e7681850413adab7d8e446577abae010a327c
Noneedtostop <125079139+Noneedtostop@users.noreply.github.com>
2024-05-14 11:36:53 -0500

    Update Second.lua

--- Second.lua
+++ Second.lua
@@ -3,47 +3,70 @@ local ScreenGui = Instance.new("ScreenGui")
 local Aimbot = Instance.new("Frame")
 local Toggle = Instance.new("TextButton")
 local Title = Instance.new("TextLabel")
+local Destroy = Instance.new("TextButton")
 
 -- Properties:
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-Aimbot.Name = "Aimbot"
 Aimbot.Parent = ScreenGui
-Aimbot.BackgroundColor3 = Color3.fromRGB(65,65,65)
-Aimbot.BorderSizePixel = 0
-Aimbot.Position = UDim2.new(0, 1167,0, 212)
-Aimbot.Size = UDim2.new(0, 95, 0, 98)
+Aimbot.BackgroundColor3 = Color3.fromRGB(65, 65, 65)
+Aimbot.BorderColor3 = Color3.fromRGB(255, 255, 255)
+Aimbot.BorderSizePixel = 3
+Aimbot.Position = UDim2.new(0, 1122, 0, 212)
+Aimbot.Size = UDim2.new(0, 200, 0, 125)
+
+Destroy.Name = "Destroy"
+Destroy.Parent = Frame
+Destroy.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Destroy.BorderColor3 = Color3.fromRGB(255, 255, 255)
+Destroy.BorderSizePixel = 3
+Destroy.Position = UDim2.new(0, 0, 0.75999999, 0)
+Destroy.Size = UDim2.new(0, 200, 0, 30)
+Destroy.Font = Enum.Font.SourceSansBold
+Destroy.TextColor3 = Color3.fromRGB(255, 255, 255)
+Destroy.TextScaled = true
+Destroy.TextSize = 14.000
+Destroy.TextWrapped = true
 
 Toggle.Name = "Toggle"
-Toggle.Parent = Aimbot
-Toggle.BackgroundColor3 = Color3.fromRGB(65,65,65)
+Toggle.Parent = Frame
+Toggle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Toggle.BackgroundTransparency = 1.000
+Toggle.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Toggle.BorderSizePixel = 0
-Toggle.Position = UDim2.new(0, 0, 0.386523575, 0)
-Toggle.Size = UDim2.new(0, 95, 0, 50)
-Toggle.Font = Enum.Font.SourceSans
+Toggle.Position = UDim2.new(0, 0, 0.296000004, 0)
+Toggle.Size = UDim2.new(0, 200, 0, 50)
+Toggle.Font = Enum.Font.SourceSansBold
 Toggle.Text = "Off"
 Toggle.TextColor3 = Color3.fromRGB(255, 0, 0)
-Toggle.TextSize = 40.000
+Toggle.TextScaled = true
+Toggle.TextSize = 14.000
 Toggle.TextWrapped = true
 
-Title.Name = "Title"
-Title.Parent = Aimbot
-Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Title.BackgroundTransparency = 1.000
-Title.BorderSizePixel = 0
-Title.Size = UDim2.new(0, 95, 0, 29)
-Title.Font = Enum.Font.SourceSans
-Title.Text = "Aimbot"
-Title.TextColor3 = Color3.fromRGB(255, 255, 255)
-Title.TextScaled = true
-Title.TextSize = 14.000
-Title.TextWrapped = true
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Size = UDim2.new(0, 200, 0, 37)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "SO GOATED"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
 
 -- Variables:
 local isRightMouseDown = false
 
 -- Functions:
+-- Function to destroy the Aimbot frame
+local function DestroyAimbot()
+	Aimbot:Destroy()
+end
+
+
 local function getClosestPlayer()
 	local closestDist = math.huge
 	local closestPlayer = nil
@@ -91,6 +114,7 @@ Toggle.MouseButton2Up:Connect(function()
 	isRightMouseDown = false
 end)
 
+Destroy.MouseButton1Click:Connect(DestroyAimbot)
 
 game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessedEvent)
     if not gameProcessedEvent and Toggle.Text == "On" then
